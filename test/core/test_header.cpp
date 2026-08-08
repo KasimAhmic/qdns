e8d12c98ac62876bb15b24a25f4eaa8f78728371
Kasim Ahmic <kasim.ahmic@gmail.com>
2026-08-08 12:36:40 -0400

    test: Add tests

--- test/core/test_header.cpp
+++ test/core/test_header.cpp
@@ -1,7 +1,9 @@
+#include "core/header.hpp"
+
 #include <array>
+#include <cstdint>
 
 #include "../testing.hpp"
-#include "core/header.hpp"
 
 TEST(HeaderTest, FailsToParseTruncatedData) {
   const std::array<std::byte, 11> data{};
@@ -39,10 +41,45 @@ TEST(HeaderTest, ParsesValidData) {
   ASSERT_TRUE(result.has_value());
 
   const auto &header = result.value();
-  EXPECT_EQ(header.getId(), 0x1234);
-  EXPECT_EQ(header.getFlags(), 0x5678);
-  EXPECT_EQ(header.getQdCount(), 1);
-  EXPECT_EQ(header.getAnCount(), 2);
-  EXPECT_EQ(header.getNsCount(), 3);
-  EXPECT_EQ(header.getArCount(), 4);
+  EXPECT_EQ(header.get_id(), 0x1234);
+  EXPECT_EQ(header.get_flags(), 0x5678);
+  EXPECT_EQ(header.get_question_count(), 1);
+  EXPECT_EQ(header.get_answer_count(), 2);
+  EXPECT_EQ(header.get_authority_count(), 3);
+  EXPECT_EQ(header.get_additional_records(), 4);
 }
+
+class HeaderOpCodeTest : public testing::TestWithParam<std::pair<uint16_t, q::dns::OpCode>> {};
+
+TEST_P(HeaderOpCodeTest, ExtractsOpCodeFromFlags) {
+  const auto &[flags, expectedOpCode] = GetParam();
+
+  const q::dns::Header header(0, flags, 0, 0, 0, 0);
+  EXPECT_EQ(header.get_op_code(), expectedOpCode);
+}
+
+INSTANTIATE_TEST_SUITE_P(HeaderOpCodeTests,
+                         HeaderOpCodeTest,
+                         testing::Values(std::make_pair(0x0000, q::dns::OpCode::Query),
+                                         std::make_pair(0x0800, q::dns::OpCode::IQuery),
+                                         std::make_pair(0x1000, q::dns::OpCode::Status),
+                                         std::make_pair(0x2000, q::dns::OpCode::Notify),
+                                         std::make_pair(0x2800, q::dns::OpCode::Update)));
+
+class HeaderResponseCodeTest : public testing::TestWithParam<std::pair<uint16_t, q::dns::ResponseCode>> {};
+
+TEST_P(HeaderResponseCodeTest, ExtractsResponseCodeFromFlags) {
+  const auto &[flags, expectedResponseCode] = GetParam();
+
+  const q::dns::Header header(0, flags, 0, 0, 0, 0);
+  EXPECT_EQ(header.get_response_code(), expectedResponseCode);
+}
+
+INSTANTIATE_TEST_SUITE_P(HeaderResponseCodeTests,
+                         HeaderResponseCodeTest,
+                         testing::Values(std::make_pair(0x0000, q::dns::ResponseCode::NoError),
+                                         std::make_pair(0x0001, q::dns::ResponseCode::FormatError),
+                                         std::make_pair(0x0002, q::dns::ResponseCode::ServerFailure),
+                                         std::make_pair(0x0003, q::dns::ResponseCode::NameError),
+                                         std::make_pair(0x0004, q::dns::ResponseCode::NotImplemented),
+                                         std::make_pair(0x0005, q::dns::ResponseCode::Refused)));
